31916a95fc5fa7575c071bea03c024c2f92a08d3
Sheng-Bo Chen <artalose@gmail.com>
2016-01-20 11:56:27 -0500

    0.0.3

--- descriptive.R
+++ descriptive.R
@@ -1,15 +1,34 @@
+if("foreign" %in% rownames(installed.packages()) == FALSE) {install.packages("foreign")}
+if("JGR" %in% rownames(installed.packages()) == FALSE) {install.packages("JGR")}
+if("Deducer" %in% rownames(installed.packages()) == FALSE) {install.packages("Deducer")}
+if("htmlTable" %in% rownames(installed.packages()) == FALSE) {install.packages("htmlTable")}
+
 library(foreign)
+library(JGR)
+library(Deducer)
 library(htmlTable)
 
-hsb1       = read.spss('hsb1.sav',to.data.frame =TRUE)
-myschool   = subset(hsb1, schoolid                                                                                                             =='1288')
+hsb1 = read.spss (
+  'hsb1.sav',
+  to.data.frame =TRUE
+)
 
-variables  = myschool[c("mathach", "ses")]
-statistics = function(x) c(mean                                                                                                                = mean(x), sd = sd(x), N = length(x))
-output     = t(sapply(variables, statistics))
+hsb1.sub = subset (
+  hsb1,
+  schoolid == 1288
+)
+
+output = descriptive.table (
+  vars = d(ses,mathach),
+  data = hsb1.sub,
+  func.names = c("Mean","St. Deviation","Valid N")
+)
+
+output = data.frame(output)
 
 htmlTable(
-	output,
-	header  =  c("mean", "sd", "N"),
-	caption = "Descriptive Statistics"
+  output,
+  header  =  c("mean", "sd", "N"),
+  caption = "Descriptive Statistics"
 )
+
