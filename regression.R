15788183020e7a4f3e4488522f656c34ea0e6c75
Sheng-Bo Chen <artalose@gmail.com>
2016-01-20 09:37:01 -0500

    2016.1.20

---
+++ regression.R
@@ -0,0 +1,15 @@
+library(foreign)
+library(htmlTable)
+
+hsb1       = read.spss('/Users/artalose/Google Drive/OSU/Courses/SP16 ESQREM 8658 - Multilevel Analys (13179)/HSB DATA/hsb1.sav',to.data.frame =TRUE)
+myschool   = subset(hsb1, schoolid                                                                                                             =='1288')
+
+variables  = myschool[c("mathach", "ses")]
+statistics = function(x) c(mean                                                                                                                = mean(x), sd = sd(x), N = length(x))
+output     = t(sapply(variables, statistics))
+
+htmlTable(
+	output,
+	header  =  c("mean", "sd", "N"),
+	caption = "Descriptive Statistics"
+)
